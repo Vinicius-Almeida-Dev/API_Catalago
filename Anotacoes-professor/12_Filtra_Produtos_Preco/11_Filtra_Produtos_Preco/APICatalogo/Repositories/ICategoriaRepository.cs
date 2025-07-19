fce7d3e56b22036b6838d5da60e4a45a82b8d18d
Vinicius-Almeida-Dev <vinicius.almeida@verisys.com.br>
2025-07-19 11:29:14 -0300

    retirando anotações

--- Anotacoes-professor/12_Filtra_Produtos_Preco/11_Filtra_Produtos_Preco/APICatalogo/Repositories/ICategoriaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using APICatalogo.Models;
-using APICatalogo.Pagination;
-
-namespace APICatalogo.Repositories;
-
-public interface ICategoriaRepository : IRepository<Categoria>
-{
-    PagedList<Categoria> GetCategorias(CategoriasParameters categoriasParams);
-}
